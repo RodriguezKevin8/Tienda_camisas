36084a3095c02edc19af10be194f8f2d3a5fbde0
oeportil <93228723+oeportil@users.noreply.github.com>
2023-09-12 10:02:21 -0600

    Aboutus.jsx

--- Tienda_camisas/src/components/Aboutus.jsx
+++ Tienda_camisas/src/components/Aboutus.jsx
@@ -1,7 +1,23 @@
 function Dos() {
   return (
     <div>
+      <Navbar/>
       <h1>About Us</h1>
+      <div className="contenedor">
+            <div className="contenedor__imagen">
+              <img src={imagen} alt="imagen"/>
+            </div>            
+            <div className="contenedor__texto">
+            <p>Somos una tienda de camisas con una pasión por la moda y el estilo. Fundada en 1995, nuestra tienda se ha dedicado a proporcionar a nuestros clientes las camisas de mayor calidad y diseño.</p>
+
+            <p>Nuestra misión es vestir a nuestros clientes con camisas que no solo sean elegantes y cómodas, sino también expresen su personalidad y estilo individual. Trabajamos con diseñadores de renombre para asegurarnos de que cada prenda sea una obra maestra de la moda.</p>
+
+            <p>En Camisas Elegantes, creemos que la ropa no solo es una forma de cubrirse, sino una forma de expresarse. Queremos ayudarte a lucir y sentirte increíble en cada ocasión.</p>
+
+            <p>Estamos comprometidos a utilizar materiales de alta calidad y prácticas de fabricación éticas en la creación de nuestras camisas. Cada detalle, desde la selección de telas hasta el acabado final, es cuidadosamente supervisado para garantizar la satisfacción de nuestros clientes.</p>
+
+           </div>
+        </div>
     </div>
   );
 }
